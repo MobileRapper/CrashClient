56faaf0e16618daf2357d844d381cb6fa53441b6
MobileRapper <164081366+MobileRapper@users.noreply.github.com>
2024-11-15 13:47:14 -0600

    Update game.js

--- game.js
+++ game.js
@@ -13,12 +13,6 @@ for (let i of exitHatRewards) {
     });
 }
 
-document.addEventListener('keypress', (event) => {
-    if (/[a-zA-Z0-9]/.test(event.key)) {
-      send({ e: "logout" });
-      send({ e: "powerChange", m: 1, i: 9 });
-    }
-  });
 
 
 var keysDown = new Set();
